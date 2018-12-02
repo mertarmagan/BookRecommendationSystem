03aae04f5b8da04562dbf14882c52370f1de5f3d
mertarmagan <mertarmagan@windowslive.com>
2018-12-03 00:51:36 +0300

    Book-ratings counted and json output generated.

--- rating_counter.py
+++ rating_counter.py
@@ -4,7 +4,7 @@ import labeler as lbl
 def read_ratings():
     return pd.read_csv("./modified_ratings.csv", sep=',', low_memory=False)
 
-def main():
+def user_counter():
     ratings = read_ratings()
     users = ratings.iloc[:, 0:1]
 
@@ -18,7 +18,25 @@ def main():
         else:
             _dict[i] = x + 1
 
+    print(len(_dict.keys()))
     # print(_dict)
-    lbl.write_dict(_dict, "user-ratings-count")
+    # lbl.write_dict(_dict, "user-ratings-count")
 
-main()
+def book_counter():
+    ratings = read_ratings()
+    books = ratings.iloc[:, 1:2]
+    books_list = sorted(books["ISBN"].tolist())
+
+    _dict = {}
+
+    for i in books_list:
+        x = _dict.get(i, -1)
+        if x == -1:
+            _dict[i] = 1
+        else:
+            _dict[i] = x + 1
+
+    print(len(_dict.keys()))
+    # lbl.write_dict(_dict, "book-ratings-count")
+
+book_counter()
