a1b20aff1fddb5ea908b5d3bbf19002a61f104cb
N. Mertcan Cankara <mcankara93@gmail.com>
2018-12-03 01:22:15 +0300

    shuffle_rating.csv generated

--- rating_modifier.py
+++ rating_modifier.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import labeler as lbl
+import csv
 
 def read_ratings():
-    return pd.read_csv("./ratings.csv", sep=';', low_memory=False)
+    return pd.read_csv("./modified_ratings.csv", sep=';', low_memory=False)
 
 def main():
     ratings = read_ratings()
+
+    ratings = ratings.sample(frac=1, replace=True)
+
+    # ratings.to_csv("./shuffled_ratings.csv", index=False, encoding="utf-8", quoting=csv.QUOTE_NONE, quotechar="", escapechar="\\")
 
     isbn_dict = lbl.read_dict("./isbn-to-id.json")
     book_list = list(isbn_dict.keys())
@@ -21,5 +26,5 @@ def main():
     # print(new_df.head())
     # print(len(new_df))
     new_df.to_csv("./modified_ratings.csv", index=False, encoding="utf-8")
-
+    return
 # main()
