984009b7a2ba565a7f4f5dec431ce808523d9c08
Sarp Saatcioglu <sarpsaatci@gmail.com>
2018-12-06 00:17:34 +0300

    deviations generated, bxi values calculated

--- sarp.py
+++ sarp.py
@@ -13,10 +13,8 @@ import pandas as pd
 # bk_dev = pd.read_csv("./book_dev.csv")  # book deviation
 
 
-def generate_dev(df):
 
-    book_distinct = np.array([], dtype="int")
-    user_distinct = np.array([], dtype="int")
+def generate_dev(df, u, user_distinct, book_distinct):
 
     x_dict = {}
     y_dict = {}
@@ -24,36 +22,49 @@ def generate_dev(df):
     x_arr = []
     y_arr = []
 
-    user_distinct = np.append(user_distinct, [df.iloc[0, 0]])
-    book_distinct = np.append(book_distinct, [df.iloc[0, 1]])
+    book_dev = {}
+    user_dev = {}
 
-    for i in range(0, df.shape[0]):
-        if df.iloc[i, 0] not in user_distinct:
-            user_distinct = np.append(user_distinct, [df.iloc[i, 0]])
-        if df.iloc[i, 1] not in book_distinct:
-            book_distinct = np.append(book_distinct, [df.iloc[i, 1]])
+    x_yarr = {}
+
+    ######## compress it all #########
 
     # print(user_distinct)
     # print("-------------")
     # print(book_distinct)
 
     for item in book_distinct:
+        sum = 0
+        x_dict = {}
         for index, row in df.iterrows():
             if row["ISBN"] == item:
                 x_dict[row["User-ID"]] = row["Book-Rating"]
+                temp = row["User-ID"]
+                sum = sum + row["Book-Rating"]
+        book_dev[item] = sum/len(x_dict) - u
+        x_yarr[item] = sum/len(x_dict)
+        # print(x_dict, sum/len(x_dict))
 
+    # print(u)
+    # print(book_dev)
+    # print(x_yarr)
 
     for item in user_distinct:
+        sum = 0
+        y_dict = {}
         for index, row in df.iterrows():
             if row["User-ID"] == item:
                 y_dict[row["ISBN"]] = row["Book-Rating"]
-        print(y_dict)
-
-
+                sum = sum + row["Book-Rating"]
+        user_dev[item] = sum/len(y_dict) - u
+    
+    # print(user_dev)
 
     # print("book-rating by user", y_arr)
     # print("+++++++++")
     # print("book-rating by user", x_dict)
+
+    return user_dev, book_dev
 
 def find_mean(_dict):
     sum = 0
@@ -64,13 +75,33 @@ def find_mean(_dict):
 
     return mean
 
-def find_prediction(usr_dev, bk_dev, x, i):
-    bxi = u + usr_dev.iloc[x] + bk_dev.iloc[i]
-    sum = 0
-    for j in range(0, sim_matrix.shape[0]):
-        if not np.isnan(sim_matrix.iloc[i,j]):
-            bxj = u + usr_dev.iloc[x] + bk_dev.iloc[j]
-            # sum = sum + (sim_matrix.iloc[i,j] * (ratings_matrix.iloc[x,j] - bxj)
+def find_prediction(user_distinct, book_distinct, user_dev, book_dev, train, x, i, u):
+    bxi = u + user_dev[x] + book_dev[i]
+    
+    max1 = 0
+    max2 = 0
+
+    for item in book_distinct:
+        sim = find_similarity(train, i, item)
+        print(item, sim)
+
+    # for j in range(0, book_distinct[0]):
+    #     if i is not j:
+    #         if max2 == max1:
+    #             max2 = find_similarity(train, i, j)
+    #         else:
+    #             max1 = find_similarity(train, i, j)
+    #             if max1 < max2:
+    #                 temp = max2
+    #                 max2 = max1
+    #                 max1 = temp
+
+    # print(max1, max2)
+
+    # for j in range(0, sim_matrix.shape[0]):
+    #     if not np.isnan(sim_matrix.iloc[i,j]):
+    #         bxj = u + usr_dev.iloc[x] + bk_dev.iloc[j]
+    #         # sum = sum + (sim_matrix.iloc[i,j] * (ratings_matrix.iloc[x,j] - bxj)
 
 def find_similarity(df, x, y):
     x_dict = {}
@@ -84,7 +115,7 @@ def find_similarity(df, x, y):
         if row["ISBN"] == y:
             y_dict[row["User-ID"]] = row["Book-Rating"]
 
-    print(x_dict)
+    # print(x_dict)
     # print(y_dict)
 
     res_x_dict = {}
@@ -101,7 +132,7 @@ def find_similarity(df, x, y):
     mean_x = find_mean(x_dict)
     mean_y = find_mean(y_dict)
 
-    print(mean_x, mean_y)
+    # print(mean_x, mean_y)
 
     for i in x_dict:
         val = x_dict[i]
@@ -143,9 +174,27 @@ def find_similarity(df, x, y):
 def main():
     train = pd.read_csv("./ex_similarity/train.csv", sep=",", low_memory=False)
 
+    book_distinct = np.array([], dtype="int")
+    user_distinct = np.array([], dtype="int")
+
+    user_distinct = np.append(user_distinct, [train.iloc[0, 0]])
+    book_distinct = np.append(book_distinct, [train.iloc[0, 1]])
+
+    for i in range(0, train.shape[0]):
+        if train.iloc[i, 0] not in user_distinct:
+            user_distinct = np.append(user_distinct, [train.iloc[i, 0]])
+        if train.iloc[i, 1] not in book_distinct:
+            book_distinct = np.append(book_distinct, [train.iloc[i, 1]])
+
     u = np.average(train.iloc[:, 2])
 
-    generate_dev(train)
+    # print(generate_dev(train, u))
+
+    dev = generate_dev(train, u, user_distinct, book_distinct)
+    book_dev = dev[1]
+    user_dev = dev[0]
+
+    find_prediction(user_distinct, book_distinct, user_dev, book_dev, train, 5, 1, u)
 
     # print(u)
 
